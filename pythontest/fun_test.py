8f99ea8b47ff48559d2a685ede7347ee355b9bef
Nandu <Nandakishore4445@gmail.com>
2024-09-15 22:21:02 +0530

    "First11 commit"

--- pythontest/fun_test.py
+++ pythontest/fun_test.py
@@ -1,10 +1,10 @@
 # Define the numbers
-number1 = 5
-number2 = 10
-number3 = 15
+number11 = 5
+number21 = 10
+number31= 15
 
 # Calculate the sum
-total = number1 + number2 + number3
+total = number11 + number21 + number31
 
 # Print the result
 print("The sum of the three numbers is:", total)
